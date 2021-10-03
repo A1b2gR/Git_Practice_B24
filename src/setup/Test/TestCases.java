b46bc9395a113639e8f435bf2b91055d519a156c
arseniy <arseniygrechenkov@yahoo.com>
2021-10-03 15:29:22 -0500

    Added Wooden Spoon

---
+++ src/setup/Test/TestCases.java
@@ -0,0 +1,10 @@
+package setup.Test;
+
+public class TestCases {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Wooden Spoon");
+        }
+    }
+    
+}
--- src/setup/car.java
+++ src/setup/car.java
@@ -1,24 +1,9 @@
 package setup;
 
 public class car {
-<<<<<<< HEAD
-//    car from master branch
-
+//
 //            we will have a conflict
 
 //    problems//
 }
-=======
-    //car form james
-    
-    ///sff
-//    sdfsdfsdfsdfsd
-//
-//
-//
-//
-//
-//
-//  }
-}
->>>>>>> James
+
